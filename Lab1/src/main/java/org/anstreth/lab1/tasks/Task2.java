11fb839a99e6d05629ddffc7ec57d792f3855a0e
fedochet <fedochet@yandex.ru>
2016-09-09 01:44:13 +0300

    draw green cube in task2 display method

--- Lab1/src/main/java/org/anstreth/lab1/tasks/Task2.java
+++ Lab1/src/main/java/org/anstreth/lab1/tasks/Task2.java
@@ -1,6 +1,12 @@
 package org.anstreth.lab1.tasks;
 
+import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.glu.GLUquadric;
+
+import static com.jogamp.opengl.GL.*;
+import static com.jogamp.opengl.GL.GL_DEPTH_BUFFER_BIT;
+import static com.jogamp.opengl.GL2ES1.GL_PERSPECTIVE_CORRECTION_HINT;
 
 /**
  * Created by roman on 09.09.2016.
@@ -11,23 +17,28 @@ public class Task2 extends AbstractTask {
         return "Rotating";
     }
 
-    @Override
-    public void init(GLAutoDrawable drawable) {
-
-    }
-
-    @Override
-    public void dispose(GLAutoDrawable drawable) {
-
-    }
 
     @Override
     public void display(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
 
-    }
+        GL2 gl2 = drawable.getGL().getGL2();
+        gl2.glShadeModel(GL2.GL_SMOOTH);
+        gl2.glClearColor(0f, 0f, 0f, 0f);
+        gl2.glClearDepth(1.0f);
+        gl2.glEnable(GL_DEPTH_TEST);
+        gl2.glDepthFunc(GL_LEQUAL);
+        gl2.glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
-    @Override
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        gl2.glClearColor(0, 0, 0, 1);
+        gl2.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        gl.glColor3f(0,1,0);
+        GLUquadric quad = glu.gluNewQuadric();
+        gl.glPushMatrix();
+        gl.glTranslatef(10, 10, 0);
+        glut.glutWireCube(5);
+        gl.glPopMatrix();
+        glu.gluDeleteQuadric(quad);
     }
 }
