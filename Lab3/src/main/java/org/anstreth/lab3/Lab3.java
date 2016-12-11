ce48e5b90652fb480087340fa7082d7cbb0481f5
Roman Golyshev <anstreth@gmail.com>
2016-12-12 01:37:19 +0300

    add stub app for lab 3

---
+++ Lab3/src/main/java/org/anstreth/lab3/Lab3.java
@@ -0,0 +1,8 @@
+package org.anstreth.lab3;
+
+class Lab3 {
+
+    public static void main(String[] args) {
+        new Lab3GLApp().start();
+    }
+}
---
+++ Lab3/src/main/java/org/anstreth/lab3/Lab3GLApp.java
@@ -0,0 +1,9 @@
+package org.anstreth.lab3;
+
+import org.anstreth.common.AbstractOpenGLApp;
+
+class Lab3GLApp extends AbstractOpenGLApp {
+    Lab3GLApp() {
+        super("Lab 3");
+    }
+}
