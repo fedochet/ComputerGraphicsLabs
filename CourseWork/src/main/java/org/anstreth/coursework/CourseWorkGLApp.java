cb7a2f13ee0d8e74f8e300e5ca9f8071608074d9
Roman Golyshev <anstreth@gmail.com>
2016-12-27 00:00:22 +0300

    set up global drawning; color sphere to red

--- CourseWork/src/main/java/org/anstreth/coursework/CourseWorkGLApp.java
+++ CourseWork/src/main/java/org/anstreth/coursework/CourseWorkGLApp.java
@@ -1,6 +1,14 @@
 package org.anstreth.coursework;
 
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
 import org.anstreth.common.AbstractOpenGLApp;
+import org.anstreth.coursework.system.SystemController;
+
+import static com.jogamp.opengl.GL.*;
+import static com.jogamp.opengl.GL.GL_DEPTH_TEST;
+import static com.jogamp.opengl.GL.GL_ONE_MINUS_SRC_ALPHA;
 
 /*
 5. Эмиттер – сфера
@@ -14,7 +22,26 @@ import org.anstreth.common.AbstractOpenGLApp;
  */
 
 class CourseWorkGLApp extends AbstractOpenGLApp {
+    private SystemController systemController = new SystemController();
+
     CourseWorkGLApp() {
         super("Course Work");
+    }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+        GL gl2 = drawable.getGL().getGL2();
+        gl2.glEnable(GL_BLEND);
+        gl2.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        gl2.glEnable(GL_DEPTH_TEST);
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        GL2 gl2 = drawable.getGL().getGL2();
+        gl2.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        gl2.glClearColor(1, 1, 1, 0);
+
+        systemController.draw(gl2, glu, glut);
     }
 }
--- CourseWork/src/main/java/org/anstreth/coursework/system/SphereEmitter.java
+++ CourseWork/src/main/java/org/anstreth/coursework/system/SphereEmitter.java
@@ -9,6 +9,7 @@ class SphereEmitter extends SystemObject {
     private final int sphereStacks = 20;
     private final int sphereRadius = 10;
     private final int sphereSlices = 20;
+    private final double[] color = {1, 0, 0};
 
     SphereEmitter() {
         setPosition(0, 0, 0);
@@ -19,6 +20,7 @@ class SphereEmitter extends SystemObject {
     public void draw(GL2 gl2, GLU glu, GLUT glut) {
         gl2.glPushMatrix();
         gl2.glTranslated(x, y, z);
+        gl2.glColor3dv(color, 0);
         glut.glutSolidSphere(sphereRadius, sphereSlices, sphereStacks);
         gl2.glPopMatrix();
     }
